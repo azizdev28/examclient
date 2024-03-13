2e02bfd9b9a0decef290a59061aceb54e7232be4
Aziz28 <az1z.bakht1yarvich@gmail.com>
2024-03-14 04:30:12 +0500

    Home static page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,219 @@
+import Image from "next/image";
 import React from "react";
-
+import Delivery from "@/image/homeimg/Delivery.png";
+import Checkmark from "@/image/homeimg/Checkmark.png";
+import Purchase from "@/image/homeimg/Purchase.png";
+import Sprout from "@/image/homeimg/Sprout.png";
+import HomeProductSection from "@/components/homeproduct";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Avion | Home",
+  description: "Siz izlagan uy jihozlari !!!",
+};
 const Home = () => {
   return (
-    <div>
-    HomePage
-    </div>
+    <main>
+      <div className="flex justify-center  gap-12 my-6 flex-wrap	">
+        <p>Plant pots</p>
+        <p>Ceramics</p>
+        <p>Tables</p>
+        <p>Chairs</p>
+        <p>Crocery</p>
+        <p>Tableware</p>
+        <p>Cutlery</p>
+      </div>
+      {/* Hero section started */}
+      <div>
+        <section className="text-gray-600 body-font ">
+          <div className="container mx-auto  bg-[#2A254B] flex px-12 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow p-5 md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-200">
+                The furniture brand for the future, with timeless designs
+              </h1>
+              <div className="flex justify-center">
+                <button className=" mb-24  mt-10 inline-flex text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-lg">
+                  View collection
+                </button>
+              </div>
+              <p className="mb-8 leading-relaxed">
+                A new era in eco friendly furniture with Avelon, the French
+                luxury retail brand with nice fonts, tasteful colors and a
+                beautiful way to display things digitally using modern web
+                technologies.
+              </p>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img
+                className="object-cover object-center rounded"
+                alt="hero"
+                src="https://dummyimage.com/720x600"
+              />
+            </div>
+          </div>
+        </section>
+        {/* Hero section end */}
+        {/* Section next  card in info */}
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-10">
+              <h2 className="text-[24px] text-[#2A254B] tracking-widest font-medium title-font mb-1">
+                What makes our brand different{" "}
+              </h2>
+            </div>
+            <div className="flex items-center  flex-wrap ">
+              <div className=" p-4 md:w-1/4  ">
+                <div className="flex gap-3 rounded-lg h-full p-8 flex-col">
+                  <div className="flex items-center mb-3">
+                    <div className="w-6 h-6 mr-3 inline-flex items-center justify-center rounded-full  text-white flex-shrink-0">
+                      <Image
+                        src={Delivery}
+                        alt="Delivery"
+                        className="rounded-full "
+                      />
+                    </div>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    Next day as standard
+                  </h2>
+                  <div className="flex-grow">
+                    <p className="leading-relaxed text-base">
+                      Order before 3pm and get your order the next day as
+                      standard
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className=" flex flex-col  p-4 md:w-1/4 ">
+                <div className="flex gap-3 rounded-lg h-full p-8 flex-col">
+                  <div className="flex items-center mb-3">
+                    <div className="w-6 h-6 mr-3 inline-flex items-center justify-center rounded-full  text-white flex-shrink-0">
+                      <Image
+                        src={Checkmark}
+                        alt="Checkmark"
+                        className="rounded-full "
+                      />
+                    </div>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    Made by true artisans
+                  </h2>
+                  <div className="flex-grow">
+                    <p className="leading-relaxed text-base">
+                      Handmade crafted goods made with real passion and
+                      craftmanship
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 md:w-1/4">
+                <div className="flex gap-3 rounded-lg h-full p-8 flex-col">
+                  <div className="flex items-center mb-3">
+                    <div className="w-6 h-6 mr-3 inline-flex items-center justify-center rounded-full  text-white flex-shrink-0">
+                      <Image
+                        src={Sprout}
+                        alt="Sprout"
+                        className="rounded-full "
+                      />
+                    </div>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    Unbeatable prices
+                  </h2>
+                  <div className="flex-grow">
+                    <p className="leading-relaxed text-base">
+                      For our materials and quality you won’t find better prices
+                      anywhere
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 md:w-1/4">
+                <div className="flex gap-3 rounded-lg h-full p-8 flex-col">
+                  <div className="flex items-center mb-3">
+                    <div className="w-6 h-6 mr-3 inline-flex items-center justify-center rounded-full  text-white flex-shrink-0">
+                      <Image
+                        src={Purchase}
+                        alt="Purchase"
+                        className="rounded-full "
+                      />
+                    </div>
+                  </div>
+                  <h2 className="text-gray-900 text-lg title-font font-medium">
+                    Recycled packaging{" "}
+                  </h2>
+                  <div className="flex-grow">
+                    <p className="leading-relaxed text-base">
+                      We use 100% recycled packaging to ensure our footprint is
+                      manageable
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* first */}
+            </div>
+          </div>
+        </section>
+        {/* Next section product */}
+        <section>
+          <HomeProductSection />
+        </section>
+
+        <section className="text-gray-600 body-font my-12 bg-[#f9f9f9] p-24">
+          <div className="container px-5 py-24 mx-auto bg-white">
+            <div className="flex flex-col text-center w-full mb-12">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                Join the club and get the benefits
+              </h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                Sign up for our newsletter and receive exclusive offers on new
+                ranges, sales, pop up stores and more
+              </p>
+            </div>
+            <div className="flex  lg:w-1/3 w-full sm:flex-row  md:w-2/3  flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-center">
+              <div className="relative flex-grow w-full">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <button className="text-white bg-[#2A254B] border-0 py-2 px-6 focus:outline-none hover:bg-[#504691] rounded">
+                SignUp
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="text-gray-600 body-font p-24">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                From a studio in London to a global brand with over 400 outlets
+              </h1>
+              <p className="mb-8 leading-relaxed">
+                When we started Avion, the idea was simple. Make high quality
+                furniture affordable and available for the mass market.
+                Handmade, and lovingly crafted furniture and homeware is what we
+                live, breathe and design so our Chelsea boutique become the
+                hotbed for the London interior design community.
+              </p>
+              <div className="flex justify-center">
+                <button className="inline-flex ml-[-16px] text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-32">
+                  Get in touch
+                </button>
+              </div>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img
+                className="object-cover object-center  rounded"
+                alt="hero"
+                src="https://dummyimage.com/720x600"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 };
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,181 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <footer className="text-gray-600 body-font bg-[#2A254B]">
+        <div className="container px-12 py-24 mx-auto">
+          <div className="flex flex-wrap md:text-left text-center order-first">
+            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-100 tracking-widest text-sm mb-3">
+                Menu
+              </h2>
+              <nav className="list-none mb-10">
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">
+                    New arrivals{" "}
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">
+                    Best sellers{" "}
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">
+                    Recently viewed{" "}
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">
+                    Popular this week{" "}
+                  </a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-100 tracking-widest text-sm mb-3">
+                Categories
+              </h2>
+              <nav className="list-none mb-10">
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">Crockery </a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">
+                    Furniture{" "}
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">Homeware </a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">
+                    Plant pots{" "}
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">Chairs</a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">Crockery</a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-gray-100 tracking-widest text-sm mb-3">
+                Our company{" "}
+              </h2>
+              <nav className="list-none mb-10">
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">About us</a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">Vacancies</a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">
+                    Contact Us
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">Privacy</a>
+                </li>
+                <li className="mt-3">
+                  <a className="text-gray-100 hover:text-gray-800">
+                    Returns policy
+                  </a>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/2 md:w-1/2 w-full px-4">
+              <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
+                <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                  <label className="leading-7 text-lg text-gray-100">
+                    Join our mailing list
+                  </label>
+                  <input
+                    type="text"
+                    id="footer-field"
+                    name="footer-field"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+                <button className="lg:mt-2 ml-[-16px] xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                  SignUp
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#2A254B] ">
+          <div className="container px-12 py-6 mx-auto border-t-2 flex items-center sm:flex-row flex-col">
+            <p className="text-sm text-gray-100 sm:ml-6 sm:mt-0 mt-4">
+              Copyright 2022 Avion LTD
+            </p>
+            <span className="inline-flex gap-6 sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+              <a className="text-gray-100">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a className="ml-3 text-gray-100">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a className="ml-3 text-gray-100">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
 
-export default Footer
+              <a className="ml-3 text-gray-100">
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="0"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  ></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
